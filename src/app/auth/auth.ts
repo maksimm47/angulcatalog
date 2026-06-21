e9b7d16c14235e83178c3aba77036cebb16a5eaa
maksimm47 <sindexander@gmail.com>
2026-06-22 00:34:14 +0300

    Realized registration logic.

--- src/app/auth/auth.ts
+++ src/app/auth/auth.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
-import { BehaviorSubject, map, Observable } from 'rxjs';
+import { BehaviorSubject, map, Observable, switchMap } from 'rxjs';
 
 
 export type SafeUser = Omit<User, 'password'>;
@@ -11,14 +11,21 @@ export type SafeUser = Omit<User, 'password'>;
 })
 export class AuthService {
   private apiUrl = 'http://localhost:3000/users'
-  
+
   private currentUserSubject = new BehaviorSubject<SafeUser | null>(
     this.loadUserFromStorage()
   );
-  public currentUser$: Observable<SafeUser | null> =
-    this.currentUserSubject.asObservable();
-  
-  constructor(private http: HttpClient) {}
+
+  public currentUser$: Observable<SafeUser | null> = this.currentUserSubject.asObservable();
+
+  private handleAuthSuccess(user: User): SafeUser {
+    const { password: _, ...safeUser } = user;
+    localStorage.setItem('currentUser', JSON.stringify(safeUser));
+    this.currentUserSubject.next(safeUser);
+    return safeUser;
+  }
+
+  constructor(private http: HttpClient) { }
 
   private loadUserFromStorage(): SafeUser | null {
     const stored = localStorage.getItem('currentUser');
@@ -27,21 +34,30 @@ export class AuthService {
 
   login(email: string, password: string): Observable<SafeUser> {
     return this.http.get<User[]>(this.apiUrl).pipe(
-      map(users =>{
-        if (users.length === 0) throw new Error('Неверные данные');
-
+      map(users => {
         const user = users.find(u => u.email === email && u.password === password);
-
-        if (!user) throw new Error('Неверные данные')
-        
-        const {password: _, ...safeUser} = user;
-        localStorage.setItem('currentUser', JSON.stringify(safeUser));
-        this.currentUserSubject.next(safeUser);
-        return safeUser;
+        if (!user) {
+          throw new Error('Неверные данные')
+        }
+        return this.handleAuthSuccess(user)
       })
     );
   };
-  
+
+  register(email: string, password: string, name: string): Observable<SafeUser> {
+    return this.http.get<User[]>(this.apiUrl).pipe(
+      switchMap(users => {
+        const uniqueEmail = users.find(u => u.email === email);
+        if (uniqueEmail) {
+          throw new Error('Пользователь с такой почтой уже существует.');
+        }
+        return this.http.post<User>(this.apiUrl, {email, password, name, role: 'user' }).pipe(
+          map(newUser => this.handleAuthSuccess(newUser))
+        );
+      })
+    );
+  }
+
   logout(): void {
     localStorage.removeItem('currentUser');
     this.currentUserSubject.next(null);
--- src/app/pages/register-page/register-page.ts
+++ src/app/pages/register-page/register-page.ts
@@ -1,9 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../auth/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './register-page.html',
   styleUrl: './register-page.css',
 })
-export class RegisterPage {}
+export class RegisterPage {
+  name: string = ''
+  email: string = ''
+  password: string = ''
+  errorMessage: string | null | undefined
+
+  constructor(private authService: AuthService, private route: Router){}
+
+  onClickRegister(){
+    this.authService.register(this.email, this.password, this.name).subscribe({
+      next: () => this.route.navigate(['']),
+      error: (err) => this.errorMessage = err.message
+    })
+  }
+}
